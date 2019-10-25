4d2a14d25461e2e2a4d4fdada9188acd02d51e75
Daniel Caldeira <daniel.caldeira112233@gmail.com>
2019-10-25 08:47:14 +0200

    First Gade Repository
    Porting all of the Visual Studios code without any changes to the code yet. So many errors currently

---
+++ GADE Final POE/Assets/Scripts/MeleeUnit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+ class MeleeUnit: Unit
+{
+    public MeleeUnit(int x, int y, string faction) : base(x, y, 200, 2, 20, 1, faction, '*', "Swordsman")
+    {
+
+    }
+
+    public MeleeUnit(string values) : base(values)
+    {
+
+    }
+
+    //public override string Save()
+    //{
+    //    return string.Format("Melee," + x +",",{y},{health},{maxHealth},{speed},{attack},{attackRange}," +
+    //      "/n" +"{faction},{symbol},{name},{isDestroyed}");
+    //}
+
+}
---
+++ GADE Final POE/Assets/Scripts/WizardUnit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class WizardUnit : Unit
+{
+    public WizardUnit(int x, int y, string faction) : base(x, y, 150, 1, 25, 5, faction, 'W', "Merlin")
+    {
+
+    }
+
+    public WizardUnit(string values) : base(values)
+    {
+
+    }
+
+}
